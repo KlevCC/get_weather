0948fc5ff2eae42da2d9a6e0063b5d608d6c8ef4
KlevCC <122701616+KlevCC@users.noreply.github.com>
2023-03-25 19:14:02 +0200

    Update get_weather.py

--- get_weather.py
+++ get_weather.py
@@ -1,25 +1,39 @@
+import tkinter as tk
 import requests
 
-api_key = "YOUR_API_KEY"
+api_key = "3e29b52cb518a18deeb617af93382433"
 
-def get_weather():
-    while True:
-        location = input("Enter the location: ")
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=imperial"
-        response = requests.get(url)
-        data = response.json()
-        if data['cod'] == '404':
-            print("Location not found. Please try again.")
-        else:
-            description = data["weather"][0]["description"]
-            temperature_F = data["main"]["temp"]
-            temperature_C = round((temperature_F - 32) * 5/9, 2)
-            humidity = data["main"]["humidity"]
-            wind_speed = data["wind"]["speed"]
-            print(f"The weather in {location} is {description}.")
-            print(f"The temperature is {temperature_C} degrees Celsius.")
-            print(f"The humidity is {humidity}%.")
-            print(f"The wind speed is {wind_speed} mph.")
-            break
+def get_weather(city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=imperial"
+    response = requests.get(url)
+    data = response.json()
+    if data["cod"] == "404":
+        return "Error: City not found"
+    else:
+        description = data["weather"][0]["description"]
+        temperature_F = data["main"]["temp"]
+        temperature_C = round((temperature_F - 32) * 5/9, 2)
+        humidity = data["main"]["humidity"]
+        wind_speed = data["wind"]["speed"]
+        return f"The weather in {city} is {description}. The temperature is {temperature_C} degrees Celcius. The humidity is {humidity}%. The wind speed is {wind_speed} mph."
 
-get_weather()
+def search():
+    city = city_entry.get()
+    weather_text.delete("1.0", "end")
+    weather = get_weather(city)
+    weather_text.insert("end", weather)
+
+root = tk.Tk()
+root.title("Weather App")
+
+city_label = tk.Label(root, text="Enter City")
+city_entry = tk.Entry(root)
+search_button = tk.Button(root, text="Search", command=search)
+weather_text = tk.Text(root, height=10, width=50)
+
+city_label.pack()
+city_entry.pack()
+search_button.pack()
+weather_text.pack()
+
+root.mainloop()
